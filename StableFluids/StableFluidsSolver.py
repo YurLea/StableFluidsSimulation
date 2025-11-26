5c7070c79a72ab33951ea80b6f327b6c962faf26
YurLea <vsailevyrui@gmail.com>
2025-11-26 19:20:00 +0300

    boundary applying functions

--- StableFluids/StableFluidsSolver.py
+++ StableFluids/StableFluidsSolver.py
@@ -32,3 +32,21 @@ class StableFluidsSolver:
         self.obstacle_mask = obstacle_mask
         self.streams = streams
 
+    def apply_boundary_velocity_conditions(self, u, v):
+
+        for condition in self.u_velocity_conditions:
+            condition.apply_boundary_conditions(u)
+
+        for condition in self.v_velocity_conditions:
+            condition.apply_boundary_conditions(v)
+
+    def apply_boundary_pressure_conditions(self, q):
+
+        for condition in self.q_pressure_conditions:
+            condition.apply_boundary_pressure(q)
+
+    def apply_density_boundary_conditions(self, s):
+
+        for condition in self.s_density_conditions:
+            condition.apply_density_boundary_conditions(s)
+
