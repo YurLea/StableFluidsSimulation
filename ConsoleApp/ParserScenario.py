0c568e2e48a1ff49c8c5430ccc96b54bd412b5d7
YurLea <vsailevyrui@gmail.com>
2025-11-28 16:01:04 +0300

    little update

---
+++ ConsoleApp/ParserScenario.py
@@ -0,0 +1,7 @@
+class ParserScenario:
+    def __init__(self):
+        self.description = (f"Default scenario. Domain is a solid box with single velocity stream "
+                            f"along the y axis and single density source")
+
+    def parse(self, args):
+        pass
